3a044213ca53a7bcf6d02e59505c93ee657e0575
Apoorv Tiwari <apoorvt2004@gmail.com>
2025-07-09 14:08:34 +0530

    Updated

---
+++ server.js
@@ -0,0 +1,16 @@
+// server.js
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from dist
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
